5349278a4277a4588dec48040c3c338c6d0e6ead
julezKiano <jgkiano@gmail.com>
2018-01-16 19:49:17 +0300

    updated ticket schema to be independant of a transaction

---
+++ graphql/schema/db/controllers/models/ticket.js
@@ -0,0 +1,43 @@
+const mongoose      = require('mongoose');
+const Schema        = mongoose.Schema;
+
+//so mongoose can stop throwing that warning
+mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema({
+    eventId: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    currency: {
+        type: String,
+        required: true,
+        default: 'KES'
+    }
+    originalOwner: {
+        type : mongoose.Schema.Types.ObjectId, ref: 'User'
+    },
+    currentOwner: {
+        type : mongoose.Schema.Types.ObjectId, ref: 'User'
+    },
+    currentOwner: {
+        type : mongoose.Schema.Types.ObjectId, ref: 'User'
+    },
+    transactionId: {
+        type : mongoose.Schema.Types.ObjectId, ref: 'Transaction',
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+const Ticket = mongoose.model('Ticket', ticketSchema);
+module.exports = Ticket;
